b493b2216c584132f60dec3bda11b3e9f01a901c
akameco <akameco.t@gmail.com>
2016-03-30 02:42:27 +0900

    Fix process working path

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const execa = require('execa');
 
 module.exports = () => {
 	return new Promise((resolve, reject) => {
-		execa('./osx-lux').then(result => {
+		execa(`${__dirname}/osx-lux`).then(result => {
 			resolve(Number(result.stdout));
 		}).catch(e => {
 			reject(`ambient-light ${e}`);
